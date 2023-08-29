3493b4715995dcbba9b6c3734f5906e15c1bda10
Luiz Felipe <lfpereira323@gmail.com>
2023-08-29 02:18:13 -0300

    alteração main

--- Program.java
+++ Program.java
@@ -1,16 +1,35 @@
 package GeradorDeNotaFiscal.src.br.poo.gerenciadornot;
 
+import java.util.Scanner;
+
 public class Program {
 
     public static void main(String[] args) {
 
-
+        Scanner sc = new Scanner(System.in);
         NotificacaoEmail n = new NotificacaoEmail("luiz@gmail.com");
         NotificacaoApp n1 = new NotificacaoApp();
         NotificacaoPush n2 = new NotificacaoPush();
 
-        n.gerar(n.getNotificacao());
-        n1.gerar(n.getNotificacao());
-        n2.gerar(n.getNotificacao());
+
+        boolean b = true;
+        while (b) {
+            System.out.println("escolha o tipo de notificação:");
+            System.out.println("1 - app");
+            System.out.println("2 - email");
+            System.out.println("3 - push");
+            System.out.println("outro número - fechar");
+            int p = sc.nextInt();
+
+            if (p == 1) {
+                n1.gerar(n.getNotificacao());
+            } else if (p == 2) {
+                n.gerar(n.getNotificacao());
+            } else if (p == 3){
+                n2.gerar(n.getNotificacao());
+            } else {
+                b = false;
+            }
+        }
     }
 }
